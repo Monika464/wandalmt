f62536858fc8f12034a58444ae9d7482dae66416
Monika Kubianka <mkubianka@gmail.com>
2025-09-28 22:00:10 +0200

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 //app.use(cors());
