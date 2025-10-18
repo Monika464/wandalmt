1e970d38961d13712e404e31c1730b94a5023aa1
Monika Kubianka <mkubianka@gmail.com>
2025-10-19 00:53:21 +0200

    Add stripe basic functions

--- src/routes/public/checkout.ts
+++ src/routes/public/checkout.ts
@@ -1,64 +1,48 @@
 import express from "express";
+import Product from "models/product.js";
 import Stripe from "stripe";
 
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 router.post("/create-checkout-session", async (req, res) => {
   try {
+    const { productId } = req.body;
+
+    // Pobierz produkt po ID
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
     const session = await stripe.checkout.sessions.create({
       ui_mode: "embedded",
       payment_method_types: ["card"],
+      mode: "payment",
       line_items: [
         {
           price_data: {
             currency: "pln",
-            product_data: { name: "Test product" },
-            unit_amount: 1000, // 10.00 PLN
+            product_data: { name: product.title },
+            unit_amount: product.price * 100,
           },
           quantity: 1,
         },
       ],
-      mode: "payment",
+      //success_url: "http://localhost:5173/success",
+      //cancel_url: "http://localhost:5173/cancel",
+
       return_url:
         "http://localhost:5173/return?session_id={CHECKOUT_SESSION_ID}",
     });
-
-    console.log("Stripe session created:", session);
+    //res.json({ id: session.id }); // zwracasz session.id
+    //console.log("Stripe session created:", session);
     res.json({ client_secret: session.client_secret });
   } catch (error) {
     console.error("Stripe error:", error);
     res.status(500).json({ error: error.message });
   }
 });
-
-// router.post("/create-checkout-session", async (req, res) => {
-//   try {
-//     const session = await stripe.checkout.sessions.create({
-//       ui_mode: "embedded",
-//       payment_method_types: ["card"],
-//       line_items: [
-//         {
-//           price_data: {
-//             currency: "pln",
-//             product_data: {
-//               name: "Test Product",
-//             },
-//             unit_amount: 200, // 2.00 PLN
-//           },
-//           quantity: 1,
-//         },
-//       ],
-//       mode: "payment",
-//       return_url:
-//         "http://localhost:5173/return?session_id={CHECKOUT_SESSION_ID}",
-//     });
-
-//     res.json({ client_secret: session.client_secret });
-//   } catch (error) {
-//     console.error("Błąd Stripe:", error);
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 router.get("/session-status", async (req, res) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
