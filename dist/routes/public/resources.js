27fb0398c8979517148b8f43631a28336d38a17a
Monika Kubianka <mkubianka@gmail.com>
2026-02-14 00:09:47 +0100

    Dist removed from repo

--- dist/routes/public/resources.js
+++
@@ -1,5 +0,0 @@
-import { getResourceByProductId } from "controllers/public/resourceControllers.js";
-import express from "express";
-const router = express.Router();
-router.get("/resources/:productId", getResourceByProductId);
-export default router;
