41679a3975ab433bed3944f3c59ee811e2223b47
Monika Kubianka <mkubianka@gmail.com>
2026-03-06 01:46:53 +0100

    Change mongoose connection

--- src/db.ts
+++ src/db.ts
@@ -5,10 +5,30 @@ const MONGO_URI =
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log("✅ Conntected to MongoDB");
+    await mongoose.connect(MONGO_URI, {
+      autoCreate: false, // 🟢 NIE twórz automatycznie baz/kolekcji
+      autoIndex: false, // 🟢 NIE twórz automatycznie indeksów
+      // maxPoolSize: 10,       // opcjonalnie
+      // serverSelectionTimeoutMS: 5000 // opcjonalnie
+    });
+    console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
     process.exit(1);
   }
 };
+
+// import mongoose from "mongoose";
+
+// const MONGO_URI =
+//   process.env.MONGO_URI || "mongodb://localhost:27017/wandaldatabase";
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(MONGO_URI);
+//     console.log("✅ Conntected to MongoDB");
+//   } catch (error) {
+//     console.error("❌ MongoDB connection error:", error);
+//     process.exit(1);
+//   }
+// };
